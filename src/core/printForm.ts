113e538599c69aa9b415e09b18b47ea7aefba0c1
Julia Kazanskaya <kazanskaya.julia@gmail.com>
2023-11-16 18:13:09 +0300

    update printForm for ts

--- src/core/printForm.ts
+++ src/core/printForm.ts
@@ -1,14 +1,14 @@
 export const printForm = (event: Event) => {
-  let form = [...(event.target as HTMLFormElement).elements] as Element[];
+  const currentInput = event.target as HTMLFormElement;
+  let form = [...(currentInput.form as HTMLFormElement).elements] as HTMLFormElement[];
 
   let formObject: { [key: string]: string } = {};
 
-  form.forEach((item: HTMLInputElement) => {
-    if (item.localName === 'input') {
-      const inputElement = item as HTMLInputElement;
-      formObject[inputElement.name] = inputElement.value;
+  form.forEach((item: HTMLFormElement) => {
+    if (item.localName === 'input'){
+      formObject[item.name] = item.value;
     }
   });
 
   console.log(formObject);
-};
+}
